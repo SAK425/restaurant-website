fc818706363cb11da9b05f19897a447727d901c3
SAK425 <sakshidarshane2017@gmail.com>
2024-09-04 00:11:48 +0530

    Created project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <h1>Restaurant website</h1>
+      <Layout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header';
+const Layout = () => {
+  return (
+    <>
+    <div>Layout</div>
+    <Header/>
+    </>
+  )
+}
+
+export default Layout
